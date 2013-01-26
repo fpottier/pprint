0306b9a0bbe9fb6a749a524fd9dd9454e221b48e
François Pottier <francois.pottier@inria.fr>
2013-01-26 14:10:05 +0100

    Removed the redundant RENDERER.

--- PPrintEngine.ml
+++ PPrintEngine.ml
@@ -134,32 +134,6 @@ type document =
 
 (* ------------------------------------------------------------------------- *)
 
-(* A signature for document renderers. *)
-
-module type RENDERER = sig
-  
-  (* Output channels. *)
-
-  type channel
-
-  (* [pretty rfrac width channel document] pretty-prints the document
-     [document] to the output channel [channel]. The parameter [width] is the
-     maximum number of characters per line. The parameter [rfrac] is the
-     ribbon width, a fraction relative to [width]. The ribbon width is the
-     maximum number of non-indentation characters per line. *)
-
-  val pretty: float -> int -> channel -> document -> unit
-
-  (* [compact channel document] prints the document [document] to the output
-     channel [channel]. No indentation is used. All newline instructions are
-     respected, that is, no groups are flattened. *)
-
-  val compact: channel -> document -> unit
-
-end
-
-(* ------------------------------------------------------------------------- *)
-
 (* The pretty rendering algorithm: preliminary declarations. *)
 
 (* The renderer is supposed to behave exactly like Daan Leijen's, although its
