7698b3fe630ac98cc08b7a366c10259bd6306e10
François Pottier <francois.pottier@inria.fr>
2014-04-03 14:38:09 +0200

    The code is simpler and more natural if the channel is wrapped within the output object (hence invisible to the user).

--- PPrintEngine.ml
+++ PPrintEngine.ml
@@ -40,9 +40,28 @@ let (<==) (x : requirement) (y : int) =
 
 (* A uniform interface for output channels. *)
 
-class type ['channel] output = object
-  method char: 'channel -> char -> unit
-  method substring: 'channel -> string -> int (* offset *) -> int (* length *) -> unit
+class type output = object
+  method char: char -> unit
+  method substring: string -> int (* offset *) -> int (* length *) -> unit
+end
+
+(* Three kinds of output channels are wrapped so as to satisfy the above
+   interface: OCaml output channels, OCaml memory buffers, and OCaml
+   formatters. *)
+
+class channel_output channel = object
+  method char = output_char channel
+  method substring = output channel
+end
+
+class buffer_output buffer = object
+  method char = Buffer.add_char buffer
+  method substring = Buffer.add_substring buffer
+end
+
+class formatter_output fmt = object
+  method char = Format.pp_print_char fmt
+  method substring = fst (Format.pp_get_formatter_output_functions fmt ())
 end
 
 (* ------------------------------------------------------------------------- *)
@@ -53,16 +72,12 @@ end
    in future versions of the library. Nevertheless, it is exposed to the user
    via [Custom] documents. *)
 
-type 'channel state = {
+type state = {
 
     (* The line width and ribbon width. *)
 
     width: int;
     ribbon: int;
-
-    (* The output channel. *)
-
-    channel: 'channel;
 
     (* The last indent. This is the number of blanks that were printed at the
        beginning of the current line. *)
@@ -93,15 +108,19 @@ class type custom = object
 
   (* The method [pretty] is used by the main rendering algorithm. It has
      access to the output channel and to the algorithm's internal state, as
-     described above. It is supposed to update the internal state in a manner
-     that is consistent with what is sent to the output channel. *)
+     described above. In addition, it receives the current indentation level
+     and the current flattening mode (on or off). If flattening mode is on,
+     then the document must be printed on a single line, in a manner that is
+     consistent with the requirement that was published ahead of time. This
+     method is supposed to update the internal state in a manner that is
+     consistent with what is sent to the output channel. *)
 
-  method pretty:  'channel . 'channel output -> 'channel state -> int -> bool -> unit
+  method pretty: output -> state -> int -> bool -> unit
 
   (* The method [compact] is used by the compact rendering algorithm. It has
      access to the output channel only. *)
 
-  method compact: 'channel . 'channel output -> 'channel -> unit
+  method compact: output -> unit
 
 end
 
@@ -381,20 +400,23 @@ type cont =
 
 (* Printing blank space (indentation characters). *)
 
+(* TEMPORARY expose these facilities in the .mli; also expose a method
+   that prints a hardline properly *)
+
 let blank_length =
   80
 
 let blank_buffer =
   String.make blank_length ' '
 
-let rec blanks output channel n =
+let rec blanks output n =
   if n <= 0 then
     ()
   else if n <= blank_length then
-    output#substring channel blank_buffer 0 n
+    output#substring blank_buffer 0 n
   else begin
-    output#substring channel blank_buffer 0 blank_length;
-    blanks output channel (n - blank_length)
+    output#substring blank_buffer 0 blank_length;
+    blanks output (n - blank_length)
   end
 
 (* ------------------------------------------------------------------------- *)
@@ -416,8 +438,8 @@ let ok state flatten : bool =
    manner in which the document is rendered. *)
 
 let rec run
-  (output: 'channel output)
-  (state : 'channel state)
+  (output : output)
+  (state : state)
   (indent : int)
   (flatten : bool)
   (doc : document)
@@ -429,25 +451,25 @@ let rec run
       continue output state cont
 
   | Char c ->
-      output#char state.channel c;
+      output#char c;
       state.column <- state.column + 1;
       (* assert (ok state flatten); *)
       continue output state cont
 
   | String (s, ofs, len) ->
-      output#substring state.channel s ofs len;
+      output#substring s ofs len;
       state.column <- state.column + len;
       (* assert (ok state flatten); *)
       continue output state cont
 
   | FancyString (s, ofs, len, apparent_length) ->
-      output#substring state.channel s ofs len;
+      output#substring s ofs len;
       state.column <- state.column + apparent_length;
       (* assert (ok state flatten); *)
       continue output state cont
 
   | Blank n ->
-      blanks output state.channel n;
+      blanks output n;
       state.column <- state.column + n;
       (* assert (ok state flatten); *)
       continue output state cont
@@ -460,8 +482,8 @@ let rec run
       (* Emit a newline character, followed by the prescribed amount of
          indentation. Update the current state to record how many indentation
          characters were printed and to to reflect the new column number. *)
-      output#char state.channel '\n';
-      blanks output state.channel indent;
+      output#char '\n';
+      blanks output indent;
       state.column <- indent;
       state.last_indent <- indent;
       continue output state cont
@@ -515,11 +537,10 @@ and continue output state = function
 
 (* This is the renderer's main entry point. *)
 
-let pretty output rfrac width channel doc =
+let pretty output rfrac width doc =
   run output {
     width = width;
     ribbon = max 0 (min width (truncate (float_of_int width *. rfrac)));
-    channel = channel;
     last_indent = 0;
     column = 0
   } 0 false doc KNil
@@ -528,73 +549,63 @@ let pretty output rfrac width channel doc =
 
 (* The compact rendering algorithm. *)
 
-let rec compact output channel = function
+let rec compact output = function
   | Empty ->
       ()
   | Char c ->
-      output#char channel c
+      output#char c
   | String (s, ofs, len) ->
-      output#substring channel s ofs len
+      output#substring s ofs len
   | FancyString (s, ofs, len, apparent_length) ->
-      output#substring channel s ofs len
+      output#substring s ofs len
   | Blank n ->
-      blanks output channel n
+      blanks output n
   | HardLine ->
-      output#char channel '\n'
+      output#char '\n'
   | Cat (_, doc1, doc2) ->
-      compact output channel doc1;
-      compact output channel doc2
+      compact output doc1;
+      compact output doc2
   | IfFlat (doc, _)
   | Nest (_, _, doc)
   | Group (_, doc)
   | Align (_, doc) ->
-      compact output channel doc
+      compact output doc
   | Custom c ->
       (* Invoke the document's custom rendering function. *)
-      c#compact output channel
+      c#compact output
 
 (* ------------------------------------------------------------------------- *)
 
-(* We now instantiate the renderers for the three kinds of output channels:
-   OCaml output channels, OCaml memory buffers, and OCaml formatters. *)
+(* We now instantiate the renderers for the three kinds of output channels. *)
 
 (* This is just boilerplate. *)
 
 module MakeRenderer (X : sig
   type channel
-  val output: channel output
+  val output: channel -> output
 end) = struct
   type channel = X.channel
   type dummy = document
   type document = dummy
-  let pretty = pretty X.output
-  let compact = compact X.output
+  let pretty rfrac width channel doc = pretty (X.output channel) rfrac width doc
+  let compact channel doc = compact (X.output channel) doc
 end
 
 module ToChannel =
   MakeRenderer(struct
     type channel = out_channel
-    let output = object
-      method char = output_char
-      method substring = output
-    end
+    let output = new channel_output
   end)
 
 module ToBuffer =
   MakeRenderer(struct
     type channel = Buffer.t
-    let output = object
-      method char = Buffer.add_char
-      method substring = Buffer.add_substring
-    end
+    let output = new buffer_output
   end)
 
 module ToFormatter =
   MakeRenderer(struct
     type channel = Format.formatter
-    let output = object
-      method char = Format.pp_print_char
-      method substring fmt = fst (Format.pp_get_formatter_output_functions fmt ())
-    end
+    let output = new formatter_output
   end)
 
