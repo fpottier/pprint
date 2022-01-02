fbf9ed81e6de690f60f20c8c16a1374753722868
François Pottier <francois.pottier@inria.fr>
2022-01-02 18:49:28 +0100

    Run [make headache].

--- bench/Size.mli
+++ bench/Size.mli
@@ -1,3 +1,16 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                    PPrint                                  *)
+(*                                                                            *)
+(*                        François Pottier, Inria Paris                       *)
+(*                              Nicolas Pouillard                             *)
+(*                                                                            *)
+(*         Copyright 2007-2022 Inria. All rights reserved. This file is       *)
+(*        distributed under the terms of the GNU Library General Public       *)
+(*        License, with an exception, as described in the file LICENSE.       *)
+(*                                                                            *)
+(******************************************************************************)
+
 (*i $Id: size.mli,v 1.5 2008-07-21 14:53:06 filliatr Exp $ i*)
 
 (* Sizes of ocaml values (in their memory representation). 
